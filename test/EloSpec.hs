0465bb28771fdfcc8ed068b4155517c59a667cef
Christopher Kolstad <chriswk@fastmail.com>
2018-06-04 22:25:00 +0200

    Cleanup test code by writing custom generator

--- test/EloSpec.hs
+++ test/EloSpec.hs
@@ -4,11 +4,13 @@ import           Elo
 import           Test.Hspec
 import           Test.QuickCheck
 
-data Game = Game { winnerRating :: Int, loserRating :: Int } deriving (Show, Eq)
+data Game = Game { winnerRating :: Float, loserRating :: Float } deriving (Show, Eq)
 
 instance Arbitrary Game where
-    arbitrary = Game <$> choose (100, 5000) <*> choose (100, 5000)
+    arbitrary = Game <$> randFloat 100 5000 <*> randFloat 100 5000
 
+randFloat :: Int -> Int -> Gen Float
+randFloat min max = fromIntegral <$> choose (min, max)
 
 
 
@@ -17,22 +19,21 @@ spec =
         it "is a zero sum game" $ property $
             \game ->
                 let
-                    winnerRatingF = fromIntegral $ winnerRating game
-                    loserRatingF = fromIntegral $ loserRating game
-                    (newWinnerRating, newLoserRating) = elo winnerRatingF loserRatingF
-                in winnerRatingF + loserRatingF == newWinnerRating + newLoserRating
+                    win = winnerRating game
+                    lose = loserRating game
+                    (newWinnerRating, newLoserRating) = elo win lose
+                    in win + lose == newWinnerRating + newLoserRating
         it "A player with a higher rating gets a lower delta from a win" $ property $
             \game ->
                 let
                     delta :: Float -> Float -> Float
                     delta old new = abs $ new - old
-                    winnerRatingF = fromIntegral $ winnerRating game
-                    loserRatingF = fromIntegral $ loserRating game
-                    (newWinnerRating, newLoserRating) = elo winnerRatingF loserRatingF
-                    (losersWinnerRating, winnersLoserRating) = elo loserRatingF winnerRatingF
-                in if winnerRatingF > loserRatingF then
-                    delta winnerRatingF newWinnerRating < delta loserRatingF losersWinnerRating
-                   else
-                    delta winnerRatingF newWinnerRating > delta loserRatingF losersWinnerRating
-
+                    win = winnerRating game
+                    lose = loserRating game
+                    (newWinnerRating, newLoserRating) = elo win lose
+                    (losersWinnerRating, winnersLoserRating) = elo lose win
+                    in if win > lose then
+                        delta win newWinnerRating < delta lose losersWinnerRating
+                       else
+                        delta win newWinnerRating > delta lose losersWinnerRating
 
